e9ecf7c145c74c3f95a29bc2aedf025fe52ba68b
mmaguire21 <makennamaguire-maguire@uiowa.edu>
2021-11-30 21:11:24 -0600

    added form to the end for name and email need to put calendar in with component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import Layout from '../components/layout'
-
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
@@ -8,7 +8,11 @@ const IndexPage = () => {
       <p>Broken Engineers Doodle Poll</p>
       <label> Looking for your next poll look no further! </label>
 
-
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+        //src = "https://www.dreamstime.com/stock-illustration-poll-icon-logo-illustration-group-pictogram-isolated-white-image92993577"
+      />
   
       </Layout></>
   )
--- src/pages/polls.js
+++ src/pages/polls.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
+/*
 function MyApp() {
   const [value, onChange] = useState(new Date());
+
 
   return (
     <div>
@@ -15,9 +17,38 @@ function MyApp() {
       />
     </div>
   );
+  */
+
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {    this.setState({value: event.target.value});  }
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name/Email:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
-/*class Calendar1 extends React.Component {
+  
+
+/*class Calendar extends React.Component {
   render() {
     <div>
       <Calendar
@@ -31,4 +62,4 @@ function MyApp() {
 }
 */
 
-export default MyApp 
+export default NameForm 
