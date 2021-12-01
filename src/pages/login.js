f331b72ed0177e25383d4f0e00934027f017cae0
Milosh98 <matthieustogsdill@gmail.com>
2021-11-30 21:11:04 -0600

    Admin Authentication implemented

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,16 +1,19 @@
 import * as React from "react"
 import Layout from '../components/layout'
+import { Link } from 'gatsby'
 
+//Hard Coded Admin Username and Password
 var uadmin = "Matthieu";
 var padmin = "Stogsdill";
 
+//Setting up an empty state to be filled with the User entered Username and Password
 export default class IndexPage extends React.Component {
   state = {
     Username: "",
     Password: "",
   }
   
-
+  //InputChange Function
   handleInputChange = event => {
     const target = event.target
     const value = target.value
@@ -19,11 +22,16 @@ export default class IndexPage extends React.Component {
       [name]: value,
     })
   }
+  
+  //Submit Function
   handleSubmit = event => {
     event.preventDefault()
+    //Compares the user entered Username and Password Strings and if they are equal sends a welcome alert
     if (uadmin.localeCompare(this.state.Username)==0 && padmin.localeCompare(this.state.Password)==0) {
-    alert(`Welcome ${this.state.Username} ${this.state.Password}!`)
+    //alert(`Welcome ${this.state.Username} ${this.state.Password}!`)
+    window.location.replace("/admin/home")
     }
+    //If they are not equal sends an Incorrect Login Alert
     else 
     alert('Incorrect Login')
   }
