5a4743fb0a09919d6fd2910164516ef67bd613a9
Milosh98 <matthieustogsdill@gmail.com>
2021-11-30 19:00:27 -0600

    testing forms

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,6 +2,10 @@ import * as React from "react"
 import Layout from '../components/layout'
 
 export default class IndexPage extends React.Component {
+  state = {
+    firstName: "",
+    lastName: "",
+  }
   render() {
     return <div>Hello world!</div>
   }
